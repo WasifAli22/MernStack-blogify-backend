6ba41c2f2e87557ff0be29c871d1d55ae7faa32c
Wasif Shahid <wasifaliwasif22@gmail.com>
2024-07-02 22:02:10 +0500

    completed category. crated all api routes

--- controllers/categories/category.js
+++ controllers/categories/category.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Category = require("../../model/category/Category");
 
-// @desc    Get all categories
+// @desc    Create category
 exports.createCategory = asyncHandler(async (req, res) => {
     const { name, author } = req.body;
     //! if exist
@@ -20,3 +20,50 @@ exports.createCategory = asyncHandler(async (req, res) => {
       });
     });
     
+//@desc  Get all Categories
+//@route GET /api/v1/categories
+//@access PUBLIC
+
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find({});
+  res.status(201).json({
+    status: "success",
+    message: "Categoryies successfully fetched",
+    categories,
+  });
+});
+
+
+//@desc  Delete Category
+//@route DELETE /api/v1/categories/:id
+//@access Private
+
+exports.deleteCategory = asyncHandler(async (req, res) => {
+  await Category.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    status: "success",
+    message: "Categoryies successfully deleted",
+  });
+});
+
+//@desc  update Category
+//@route PUT /api/v1/categories/:id
+//@access Private
+
+exports.updateCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(201).json({
+    status: "success",
+    message: "Categoryies successfully updated",
+    category,
+  });
+});
--- routes/category/categoryRouter.js
+++ routes/category/categoryRouter.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createCategory } = require("../../controllers/categories/category");
+const { createCategory,getCategories,updateCategory,deleteCategory } = require("../../controllers/categories/category");
 const isLoggin = require("../../middlewares/isLoggin");
 const categoryRouter = express.Router();
 
-
+// create category
 categoryRouter.post("/",isLoggin, createCategory);
+
+// get all categories
+categoryRouter.get("/", getCategories);
+// delete category
+categoryRouter.delete("/:id", isLoggin , deleteCategory);
+
+// update category
+categoryRouter.put("/:id", updateCategory);
 module.exports = categoryRouter;
