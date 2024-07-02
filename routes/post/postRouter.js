170436d85663905c0b4d848aa96a00ab7b03fcfe
Wasif Shahid <wasifaliwasif22@gmail.com>
2024-07-03 01:13:26 +0500

    completed post CRUD operations

--- routes/post/postRouter.js
+++ routes/post/postRouter.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const isLoggin = require("../../middlewares/isLoggin");
-const { createPost } = require("../../controllers/posts/posts");
+const { createPost, getPosts, getPost, updatePost, deletePost } = require("../../controllers/posts/posts");
 const postsRouter = express.Router();
 
 // create
 postsRouter.post("/", isLoggin, createPost )
+// get all posts
+postsRouter.get("/", getPosts )
+// get single post
+postsRouter.get("/:id", getPost )
+// update post
+postsRouter.put("/:id",isLoggin, updatePost )
+// delete post
+postsRouter.delete("/:id",isLoggin, deletePost )
 
 
 module.exports = postsRouter;
