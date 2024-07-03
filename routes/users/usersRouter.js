c4e76603d84c15b0ace9e69928a637065e66fb30
Wasif Shahid <wasifaliwasif22@gmail.com>
2024-07-03 17:16:09 +0500

    block and unblock functionality implemented

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,4 +1,4 @@
-const { register, login, getProfile } = require("../../controllers/users/UsersCtrl");
+const { register, login, getProfile, blockUser, unblockUser } = require("../../controllers/users/UsersCtrl");
 const express = require("express");
 const usersRouter = express.Router();
 isLoggin = require("../../middlewares/isLoggin");
@@ -10,5 +10,9 @@ usersRouter.post('/login', login);
 // profile route
 // isLoggin is a middleware that checks if the user is logged in or not before proceeding to the next function in the route handler
 usersRouter.get('/profile/', isLoggin, getProfile);
+// block user 
+usersRouter.put('/block/:userIdToBlock', isLoggin, blockUser);
+// Unblock user 
+usersRouter.put('/unblock/:userIdToUnBlock', isLoggin, unblockUser);
 
 module.exports = usersRouter;
